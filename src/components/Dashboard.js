db5ec75f3a8dd4bd5e3f46eac5b2b01eb4d5a36a
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-01-11 16:51:56 -0600

    added logo and navbar, not yet completed

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,11 +2,23 @@ import { useState, useEffect, useCallback } from 'react'
 import useNavigate from 'react-router-dom'
 
 
-const Dashboard = (props) => {
+const Dashboard = () => {
 
     return (
         <>
+            {/* Dashboard Page */}
+            <div className="container-fluid text-center w-100 mx-auto mt-5">
+                <div>
+                    <h1>Your Trip Plans</h1>
+                    <button>Plan New Trip</button>
+                </div>
 
+                {/* dashboard items */}
+                <div>
+
+                </div>
+
+            </div>
         </>
     )
 }
