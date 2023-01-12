22f5f8114f2db11e049daadef74d8a7d33b0e319
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-01-12 14:42:19 -0600

    organized the cards so that it'd be 2 per row

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -14,66 +14,68 @@ const Dashboard = () => {
                 </div>
 
                 {/* dashboard list */}
-                <div className="d-flex justify-content-center dash-list">
+                <div className="container-fluid d-flex dash-list">
+                    <div className="row justify-content-center">
                     {/* dashboard items */}
-                    <div className='col-md-6 m-3 card dash-card border d-flex shadow-sm rounded'>
-                        <img className="trip-image-dash" src="https://petkeen.com/wp-content/uploads/2020/06/black-chantilly-tiffany-cat-home_Anna-Krivitskaya_Shutterstock.jpg" />
-                        <div className="d-flex flex-row justify-content-around py-4">
-                            <div className="">
-                                <h3>Place Name</h3>
-                            </div>
-                            
-                            <div className="text-left">
-                                <h4>Dates</h4>
-                                <h4>Estimated Budget</h4>
-                            </div>
-                        </div>
-
-                    </div>
-                    <div className='col-md-6 m-3 card dash-card border d-flex shadow-sm rounded'>
-                        <img className="trip-image-dash" src="https://petkeen.com/wp-content/uploads/2020/06/black-chantilly-tiffany-cat-home_Anna-Krivitskaya_Shutterstock.jpg" />
-                        <div className="d-flex flex-row justify-content-around py-4">
-                            <div className="">
-                                <h3>Place Name</h3>
-                            </div>
-                            
-                            <div className="text-left">
-                                <h4>Dates</h4>
-                                <h4>Estimated Budget</h4>
+                        <div className='m-3 card dash-card border d-flex shadow-sm rounded'>
+                            <img className="trip-image-dash" src="https://petkeen.com/wp-content/uploads/2020/06/black-chantilly-tiffany-cat-home_Anna-Krivitskaya_Shutterstock.jpg" />
+                            <div className="d-flex p-3 flex-row justify-content-around py-4">
+                                <div className="text-left ">
+                                    <h3>Place Name</h3>
+                                </div>
+                                
+                                <div className="text-left">
+                                    <h4>Dates</h4>
+                                    <h4>Estimated Budget</h4>
+                                </div>
                             </div>
                         </div>
 
-                    </div>
-                    <div className='col-md-6 m-3 card dash-card border d-flex shadow-sm rounded'>
-                        <img className="trip-image-dash" src="https://petkeen.com/wp-content/uploads/2020/06/black-chantilly-tiffany-cat-home_Anna-Krivitskaya_Shutterstock.jpg" />
-                        <div className="d-flex flex-row justify-content-around py-4">
-                            <div className="">
-                                <h3>Place Name</h3>
+                        
+                        <div className='m-3 card dash-card border d-flex shadow-sm rounded'>
+                            <img className="trip-image-dash" src="https://petkeen.com/wp-content/uploads/2020/06/black-chantilly-tiffany-cat-home_Anna-Krivitskaya_Shutterstock.jpg" />
+                            <div className="d-flex p-3 flex-row justify-content-around py-4">
+                                <div className="text-left">
+                                    <h3>Place Name</h3>
+                                </div>
+                                
+                                <div className="text-left">
+                                    <h4>Dates</h4>
+                                    <h4>Estimated Budget</h4>
+                                </div>
                             </div>
-                            
-                            <div className="text-left">
-                                <h4>Dates</h4>
-                                <h4>Estimated Budget</h4>
+
+                        </div>
+                        <div className='m-3 card dash-card border d-flex shadow-sm rounded'>
+                            <img className="trip-image-dash" src="https://petkeen.com/wp-content/uploads/2020/06/black-chantilly-tiffany-cat-home_Anna-Krivitskaya_Shutterstock.jpg" />
+                            <div className="d-flex p-3 flex-row justify-content-around py-4">
+                                <div className="text-left">
+                                    <h3>Place Name</h3>
+                                </div>
+                                
+                                <div className="text-left">
+                                    <h4>Dates</h4>
+                                    <h4>Estimated Budget</h4>
+                                </div>
                             </div>
+
+                        </div>
+                        <div className='m-3 card dash-card border d-flex shadow-sm rounded'>
+                            <img className="trip-image-dash" src="https://petkeen.com/wp-content/uploads/2020/06/black-chantilly-tiffany-cat-home_Anna-Krivitskaya_Shutterstock.jpg" />
+                            <div className="d-flex p-3 flex-row justify-content-around py-4">
+                                <div className="text-left">
+                                    <h3>Place Name</h3>
+                                </div>
+                                
+                                <div className="text-left">
+                                    <h4>Dates</h4>
+                                    <h4>Estimated Budget</h4>
+                                </div>
+                            </div>
+
                         </div>
 
                     </div>
-                    <div className='col-md-6 m-3 card dash-card border d-flex shadow-sm rounded'>
-                        <img className="trip-image-dash" src="https://petkeen.com/wp-content/uploads/2020/06/black-chantilly-tiffany-cat-home_Anna-Krivitskaya_Shutterstock.jpg" />
-                        <div className="d-flex flex-row justify-content-around py-4">
-                            <div className="">
-                                <h3>Place Name</h3>
-                            </div>
-                            
-                            <div className="text-left">
-                                <h4>Dates</h4>
-                                <h4>Estimated Budget</h4>
-                            </div>
-                        </div>
-
-                    </div>
-
-                    
                 </div>
 
             </div>
