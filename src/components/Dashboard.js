73183922f2b1e5cf59db33e0b7ce7a2f7bba84a7
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-01-11 23:00:42 -0600

    trying to cards on dash to be 2 cards per line

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -7,19 +7,73 @@ const Dashboard = () => {
     return (
         <>
             {/* Dashboard Page */}
-            <div className="container-fluid text-center w-100 mx-auto mt-5">
-                <div>
+            <div className="container-fluid w-100 mx-auto mt-5">
+                <div className="text-center">
                     <h1>Your Trip Plans</h1>
                     <button className="btn btn-warning my-5"> + Plan a New Trip</button>
                 </div>
 
-                {/* dashboard items */}
-                <div className="d-flex justify-content-center">
-                    <div className='card border shadow-sm rounded w-50'>
-                        <img />
-                        <h2>Place Name</h2>
+                {/* dashboard list */}
+                <div className="d-flex justify-content-center dash-list">
+                    {/* dashboard items */}
+                    <div className='col-md-6 m-3 card dash-card border d-flex shadow-sm rounded'>
+                        <img className="trip-image-dash" src="https://petkeen.com/wp-content/uploads/2020/06/black-chantilly-tiffany-cat-home_Anna-Krivitskaya_Shutterstock.jpg" />
+                        <div className="d-flex flex-row justify-content-around py-4">
+                            <div className="">
+                                <h3>Place Name</h3>
+                            </div>
+                            
+                            <div className="text-left">
+                                <h4>Dates</h4>
+                                <h4>Estimated Budget</h4>
+                            </div>
+                        </div>
 
                     </div>
+                    <div className='col-md-6 m-3 card dash-card border d-flex shadow-sm rounded'>
+                        <img className="trip-image-dash" src="https://petkeen.com/wp-content/uploads/2020/06/black-chantilly-tiffany-cat-home_Anna-Krivitskaya_Shutterstock.jpg" />
+                        <div className="d-flex flex-row justify-content-around py-4">
+                            <div className="">
+                                <h3>Place Name</h3>
+                            </div>
+                            
+                            <div className="text-left">
+                                <h4>Dates</h4>
+                                <h4>Estimated Budget</h4>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className='col-md-6 m-3 card dash-card border d-flex shadow-sm rounded'>
+                        <img className="trip-image-dash" src="https://petkeen.com/wp-content/uploads/2020/06/black-chantilly-tiffany-cat-home_Anna-Krivitskaya_Shutterstock.jpg" />
+                        <div className="d-flex flex-row justify-content-around py-4">
+                            <div className="">
+                                <h3>Place Name</h3>
+                            </div>
+                            
+                            <div className="text-left">
+                                <h4>Dates</h4>
+                                <h4>Estimated Budget</h4>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className='col-md-6 m-3 card dash-card border d-flex shadow-sm rounded'>
+                        <img className="trip-image-dash" src="https://petkeen.com/wp-content/uploads/2020/06/black-chantilly-tiffany-cat-home_Anna-Krivitskaya_Shutterstock.jpg" />
+                        <div className="d-flex flex-row justify-content-around py-4">
+                            <div className="">
+                                <h3>Place Name</h3>
+                            </div>
+                            
+                            <div className="text-left">
+                                <h4>Dates</h4>
+                                <h4>Estimated Budget</h4>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    
                 </div>
 
             </div>
