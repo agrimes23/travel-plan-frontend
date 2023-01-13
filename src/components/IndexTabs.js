e92e106332fd88968c9bfdca30963d408461f276
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-01-12 21:30:34 -0600

    just started making an index component that will show the options each day of the trip

---
+++ src/components/IndexTabs.js
@@ -0,0 +1,23 @@
+import { useState, useEffect, useCallback } from 'react'
+import useNavigate from 'react-router-dom'
+
+
+const IndexTabs = () => {
+
+    return (
+        <>
+            {/* Tabs */}
+            <div>
+
+            </div>
+
+            {/* Body of Index Cards */}
+            <div>
+
+            </div>
+
+        </>
+    )
+}
+    
+export default IndexTabs;
