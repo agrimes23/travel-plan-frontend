48b1fb7ab6122c34eb96d52848d5f1a1c2774080
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-01-23 01:10:10 -0600

    added heroku backend

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
 
   // get user info + all saved plans
   const getUserPlans = (data) => {
-    axios.get('http://localhost:8080/api/v1/userplans/' + data.id)
+    axios.get('https://travel-planning-backend.herokuapp.com/api/v1/userplans/' + data.id)
     .then((res) => setUserPlans(res.data),
     (err) => console.log(err),
     
@@ -25,14 +25,14 @@ function App() {
 
   // create new user in db
   const handleCreate = (addUser) => {
-    axios.post('http://localhost:8080/api/v1/userplans', addUser)
+    axios.post('https://travel-planning-backend.herokuapp.com/api/v1/userplans', addUser)
     .then((res) => {
       getUserPlans(res.data)
     })
   }
 
   const handleLogin = (data) => {
-    axios.post('http://localhost:8080/api/v1/userplans/login', data)
+    axios.post('https://travel-planning-backend.herokuapp.com/api/v1/userplans/login', data)
     .then(response => setUserPlans(response.data))
   }
 
