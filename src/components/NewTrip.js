0c7ea1f37857ab652e66215b6c993ebe091c7190
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-02-17 19:28:42 -0600

    changed the navigation for different buttons on dashboard.

--- src/components/NewTrip.js
+++ src/components/NewTrip.js
@@ -3,10 +3,10 @@ import { useNavigate } from 'react-router-dom'
 
 
 const NewTrip = (props) => {
-
-    const [newTripDetails, setNewTripDetails] = useState({...props.userPlans})
-    const navigate = useNavigate();
     const newEntry = props.userPlans.tripPlans.length
+    const [newTripDetails, setNewTripDetails] = useState({...props.userPlans.tripPlans[newEntry]})
+    const navigate = useNavigate();
+    
 
     const handleChange = (e) => {
         setNewTripDetails({...newTripDetails, [e.target.name]: e.target.value})
@@ -18,7 +18,7 @@ const NewTrip = (props) => {
         navigate('/dashboard')
     }
 
-    // console.log("helloooo " + props.userPlans.tripPlans)
+    // console.log("helloooo " + props.userPlans.tripPlans[0].estBud)
 
     return (
         <>
@@ -29,19 +29,19 @@ const NewTrip = (props) => {
                         <form onSubmit={handleSubmit} className='row'>
 
                             <label htmlFor='id'>Please write: 0{newEntry + 1}</label>
-                            <input placeholder='id' name="id" value={newTripDetails.tripPlans.id} onChange={handleChange} />
+                            <input placeholder='id' name="id" value={newTripDetails.id} onChange={handleChange} />
 
                             <label htmlFor='enter trip location'>Enter Trip Location </label>
-                            <input placeholder='city, town, country etc' name="placeName" value={newTripDetails.tripPlans.placeName} onChange={handleChange} />
+                            <input placeholder='city, town, country etc' name="placeName" value={newTripDetails.placeName} onChange={handleChange} />
 
                             <label htmlFor='enter trip start date'>Enter Trip Start Date </label>
-                            <input placeholder='the day you start your trip' name="tripStart" value={newTripDetails.tripPlans.tripStart} onChange={handleChange} />
+                            <input placeholder='the day you start your trip' name="tripStart" value={newTripDetails.tripStart} onChange={handleChange} />
 
                             <label htmlFor='enter trip end date'>Enter Trip End Date </label>
-                            <input placeholder='the day you get back' name="tripEnd" value={newTripDetails.tripPlans.tripEnd} onChange={handleChange} />
+                            <input placeholder='the day you get back' name="tripEnd" value={newTripDetails.tripEnd} onChange={handleChange} />
 
                             <label htmlFor='enter estimated budget'>Enter Estimated Budget </label>
-                            <input placeholder='budget for trip' name="EstBud" value={newTripDetails.tripPlans.EstBud} onChange={handleChange} />
+                            <input placeholder='budget for trip' name="estBud" value={newTripDetails.estBud} onChange={handleChange} />
 
                             <div className="d-flex justify-content-center align-items-center">
                                 <input className="btn btn-info mt-5 " type="submit"/>
